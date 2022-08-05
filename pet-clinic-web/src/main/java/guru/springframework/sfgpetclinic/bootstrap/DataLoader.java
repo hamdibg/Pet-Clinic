b5c128217ee7cf14473c6af044e854ede111e3f2
hamdibg <37661149+hamdibg@users.noreply.github.com>
2022-08-05 12:15:19 +0100

    Enhance Owners with contact info and Pets on Startup. closes #34

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
@@ -8,6 +9,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -37,12 +40,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("265 Brooklin");
+        owner1.setCity("Brooklin");
+        owner1.setTelephone("5247653215");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthday(LocalDate.now());
+        mikesPet.setName("Rosko");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("265 Brooklin");
+        owner2.setCity("Brooklin");
+        owner2.setTelephone("5247653215");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setPetType(savedDogPetType);
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthday(LocalDate.now());
+        fionasCat.setName("samira");
+        owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
 
--- petclinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ petclinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,9 +1,10 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Owner extends Person{
-    private Set<Pet> pets;
+    private Set<Pet> pets = new HashSet<>();
     private String address;
     private String city;
     private String telephone;
